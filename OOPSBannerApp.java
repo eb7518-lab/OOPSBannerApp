b8d2522f6ad48ceffee75f85f8b2c07f1ee1abfe
eb7518-lab <eb7518@srmist.edu.in>
2026-02-18 22:27:55 +0530

    Coded to print OOPS by Static Functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,17 +2,52 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] lines = {
-            String.join(" ", "  ***   ", "  ***   ", " *****  ", " ***** "),
-            String.join(" ", " *   *  ", " *   *  ", " *    * ", " *     "),
-            String.join(" ", " *   *  ", " *   *  ", " *****  ", " ***** "),
-            String.join(" ", " *   *  ", " *   *  ", " *      ", "     * "),
-            String.join(" ", "  ***   ", "  ***   ", " *      ", " ***** ")
-        };
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        String[] lines = new String[o.length];
+
+        for (int i = 0; i < o.length; i++) {
+            lines[i] = String.join(" ", o[i], o[i], p[i], s[i]);
+        }
 
         for (String line : lines) {
             System.out.println(line);
         }
+    }
+
+    // Method for letter O
+    public static String[] getOPattern() {
+        return new String[]{
+            "  ***   ",
+            " *   *  ",
+            " *   *  ",
+            " *   *  ",
+            "  ***   "
+        };
+    }
+
+    // Method for letter P
+    public static String[] getPPattern() {
+        return new String[]{
+            " *****  ",
+            " *    * ",
+            " *****  ",
+            " *      ",
+            " *      "
+        };
+    }
+
+    // Method for letter S
+    public static String[] getSPattern() {
+        return new String[]{
+            " ***** ",
+            " *     ",
+            " ***** ",
+            "     * ",
+            " ***** "
+        };
     }
 }
 
